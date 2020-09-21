f6c44c7addcff806dabe43aec273080b387d6264
Aguinelo Koczkodai <aguinelo@ootz.com.br>
2020-09-21 06:39:30 -0300

    add hpa

--- go/src/main.go
+++ go/src/main.go
@@ -2,14 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"math"
+	"net/http"
 )
 
-func main() {
+func handler(w http.ResponseWriter, r *http.Request) {
 	x := 0.0001
 
 	for i := 0; i < 1000000; i++ {
 		x += math.Sqrt(x)
 	}
-	fmt.Println("Code.education Rocks!")
+	fmt.Fprintf(w, "%s", "Code.education Rocks!")
+
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
